cf6ded91a6fca96a3450b4cf517540d6b7f3cc00
lv42840-svg <lv42840@gmail.com>
2026-09-12 17:42:39 +0200

    feat: type desktop setup bridge

---
+++ src/types/desktop.d.ts
@@ -0,0 +1,28 @@
+export {};
+
+declare global {
+  interface Window {
+    papotDesktop?: {
+      saveSetup: (input: {
+        sharedDataPath: string;
+        nextcloudBaseUrl: string;
+        nextcloudLogin: string;
+        nextcloudAppPassword: string;
+        deviceLabel: string;
+        papotUserDisplayName: string;
+      }) => Promise<
+        | {
+            ok: true;
+            config: {
+              deviceId: string;
+              deviceLabel: string;
+              nextcloudUserId: string;
+              papotUserDisplayName: string;
+              sharedDataPath: string;
+            };
+          }
+        | { ok: false; error: string }
+      >;
+    };
+  }
+}
